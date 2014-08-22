24c5d5b608d8d22dd1eab48cdc38a2126600938b
thehindenburg <jblaney@003ee1c08089.ant.amazon.com>
2014-08-22 13:20:54 -0700

    Added 250 pixels

--- application.js
+++ application.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
 	height:'80px',
 		},{"queue": false});
 	$(this).find('#secondtext').animate({bottom:'15px'},{"queue": false});
-	$(this).find('#secondtext').slideDown(300);
+	$(this).find('#secondtext').slideDown(300).delay(200);
 	});
 $('.photopopup').on('mouseleave', function(){
 $(this).find('.darkline').animate({height:'50px', bottom:'50px'},{"queue": false});
 $(this).find('#secondtext').animate({bottom:'0px'},{"queue": false})
-$(this).find('#secondtext').slideUp(300);
+$(this).find('#secondtext').slideUp(300).delay(200);
 });
 });
