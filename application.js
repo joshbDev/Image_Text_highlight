3e78905e4697875ce7ad57805b7c58445ccb7081
thehindenburg <jblaney@003ee1c08089.ant.amazon.com>
2014-08-25 11:07:00 -0700

    Making text more fluid

--- application.js
+++ application.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
 	$('.photopopup').on('mouseover', function(){
 	$(this).find('.darkline').animate({
 	bottom:'80px',
-	height:'80px',
+	height:'80px'
 		},{"queue": false});
-	$(this).find('#secondtext').animate({bottom:'15px'},{"queue": false});
-	$(this).find('#secondtext').slideDown(300).delay(200);
+    		$(this).find('#secondtext').slideDown(200);
+	$(this).find('#secondtext').animate({bottom:'15px'},{"queue": false}, 500);
 	});
 $('.photopopup').on('mouseleave', function(){
 $(this).find('.darkline').animate({height:'50px', bottom:'50px'},{"queue": false});
-$(this).find('#secondtext').animate({bottom:'0px'},{"queue": false})
+$(this).find('#secondtext').animate({bottom:'-15px'},{"queue": false}, 300);
 $(this).find('#secondtext').slideUp(300).delay(200);
 });
 });
