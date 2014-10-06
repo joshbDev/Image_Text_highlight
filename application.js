0e2b71ab2a487ad5e5d3693c5fd21dab16c5f840
thehindenburg <jblaney@003ee1c08089.ant.amazon.com>
2014-10-06 13:04:56 -0700

    stop secondary text from queueing popup

--- application.js
+++ application.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$('.photopopup').on('mouseover', function(){
+		$(this).find('#secondtext').dequeue();
 	$(this).find('.darkline').animate({
 	bottom:'80px',
 	height:'80px'
@@ -8,6 +9,7 @@ $(document).ready(function() {
 	$(this).find('#secondtext').animate({bottom:'15px'},{"queue": false}, 500);
 	});
 $('.photopopup').on('mouseleave', function(){
+	$(this).find('#secondtext').dequeue();
 $(this).find('.darkline').animate({height:'50px', bottom:'50px'},{"queue": false});
 $(this).find('#secondtext').animate({bottom:'-15px'},{"queue": false}, 300);
 $(this).find('#secondtext').slideUp(300).delay(200);
